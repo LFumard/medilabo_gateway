1b6ba4aea8242c275c9e13cd418abe4aa9d32b7f
LFumard <122115111+LFumard@users.noreply.github.com>
2024-03-14 13:16:56 +0100

    Finalisation projet Gateway - Sprint 1

---
+++ src/main/java/com/lfumard/medilabo_gateway/MedilaboGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.lfumard.medilabo_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MedilaboGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboGatewayApplication.class, args);
+	}
+
+}
