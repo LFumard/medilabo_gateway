3bb692a51521840ee4453684d0a9668b2eaaedfd
LFumard <122115111+LFumard@users.noreply.github.com>
2024-04-29 12:18:04 +0200

    Ajout security

---
+++ src/main/java/com/lfumard/medilabo_gateway/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,57 @@
+package com.lfumard.medilabo_gateway.security.jwt;
+
+//import com.LFumard.medilabo.security;
+import com.lfumard.medilabo_gateway.security.jwt.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+//@RefreshScope
+@Order(Integer.MIN_VALUE)
+@Component
+public class AuthTokenFilter  extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    //private jwtUtils jwtUtils2;
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String jwt = parseJwt(request);
+        jwtUtils.validateJwtToken(jwt);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        //String jwt = request.getHeader("Authentication");
+        String jwt = request.getHeader("Authorization");
+        //jwt.replace()//String jwt = jwtUtils.getJwtFromCookies(request);
+        if (StringUtils.hasText(jwt) && jwt.startsWith("Bearer ")) {
+            return jwt.substring(7, jwt.length());
+        }
+        return null;
+    }
+}
